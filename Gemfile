67c5680e6bdd276215a0eb59b298989af3995669
root <root@deploy.(none)>
2016-03-11 16:36:20 -0500

    Final

--- Gemfile
+++ Gemfile
@@ -1,11 +1,11 @@
 source 'https://rubygems.org'
 
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.5.1'
-# Use mysql as the database for Active Record
-gem 'mysql2', '>= 0.3.13', '< 0.5'
+gem 'rails', '4.2.5.2'
+# Use sqlite3 as the database for Active Record
+#gem 'sqlite3'
 gem 'rb-readline'
+gem 'mysql2', '>= 0.3.13', '< 0.5'
 gem 'acts_as_list', '~> 0.7.2'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
@@ -13,11 +13,13 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.1.0'
+# gem 'coffee-rails'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'bcrypt', '~> 3.1', '>= 3.1.5'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'jquery-ui-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,22 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   # GET /products
+  # GET /products.json
   def index
     @products = Product.all
   end
 
   # GET /products/1
+  # GET /products/1.json
   def show
   end
 
@@ -20,29 +30,58 @@ class ProductsController < ApplicationController
   end
 
   # POST /products
+  # POST /products.json
   def create
     @product = Product.new(product_params)
 
-    if @product.save
-      redirect_to @product, notice: 'Product was successfully created.'
-    else
-      render :new
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product,
+          notice: 'Product was successfully created.' }
+        format.json { render action: 'show', status: :created,
+          location: @product }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @product.errors,
+          status: :unprocessable_entity }
+      end
     end
   end
 
   # PATCH/PUT /products/1
+  # PATCH/PUT /products/1.json
   def update
-    if @product.update(product_params)
-      redirect_to @product, notice: 'Product was successfully updated.'
-    else
-      render :edit
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product,
+          notice: 'Product was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @product.errors,
+          status: :unprocessable_entity }
+      end
     end
   end
 
   # DELETE /products/1
+  # DELETE /products/1.json
   def destroy
     @product.destroy
-    redirect_to products_url, notice: 'Product was successfully destroyed.'
+    respond_to do |format|
+      format.html { redirect_to products_url }
+      format.json { head :no_content }
+    end
+  end
+
+  def who_bought
+    @product = Product.find(params[:id])
+    @latest_order = @product.orders.order(:updated_at).last
+    if stale?(@latest_order)
+      respond_to do |format|
+        format.atom
+      end
+    end
   end
 
   private
@@ -51,7 +90,8 @@ class ProductsController < ApplicationController
       @product = Product.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
+    # Never trust parameters from the scary internet, only allow the white
+    # list through.
     def product_params
       params.require(:product).permit(:title, :description, :image_url, :price)
     end
